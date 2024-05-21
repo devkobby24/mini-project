bdadb16ecb8769c17218d97eaf1df68adad626b5
devkobby24 <nanakobby2002@gmail.com>
2024-05-21 23:04:48 +0000

    made some changes

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Weather = () => {
+  return (
+    <View>
+      <Text> Weather</Text>
+    </View>
+  )
+}
+
+export default Weather
