99469452af3ea7e0fc01b4a7aa34e3ac1fbbc8cd
devkobby24 <nanakobby2002@gmail.com>
2024-05-26 12:06:25 +0000

    update

--- App/Screen/loginScreen/LoginScreen.jsx
+++ App/Screen/loginScreen/LoginScreen.jsx
@@ -4,10 +4,21 @@ import React from 'react'
 
 export default function LoginScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{
+      display:'flex',
+      justifyContent: 'center',
+      alignItems:'center',
+    }}>
       <Image source={require('./../../../assets/images/logo1.png')} 
         style={styles.logoImage}
       />
+      <Image source={require('./../../../assets/images/evc.png')}
+        style={styles.loginbg}
+      />
+      <View style={{padding:20}}>
+        <Text style={styles.heading}>Your Ultimate EV Charging station finder App</Text>
+        <Text style={styles.desc}>Find EV charging stations near you, plan trips and so much more in just one click</Text>
+      </View>
     </SafeAreaView>
   )
 }
@@ -17,5 +28,24 @@ const styles = StyleSheet.create({
       width:200,
       height:40,
       objectFit:"contain"
-    }
+    },
+    loginbg:{
+      width:"100%",
+      height:150,
+      objectFit:"cover",
+      marginTop:0,
+    },
+    heading:{
+      fontSize:25,
+      fontFamily:'outfit-bold',
+      textAlign:'center',
+      marginTop:20
+    },
+    desc:{
+      fontSize:20,
+      fontFamily:'outfit-medium',
+      textAlign:'center',
+      marginTop:20
+    },
+
 })
