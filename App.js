376185506d1e919ad430989b5b4fa3bdbaaa1867
devkobby24 <nanakobby2002@gmail.com>
2024-05-26 10:47:22 +0000

    made some progress

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-SemiBold.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),  
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>This is the project we're collaborating on ys kay</Text>
-      <Text>Just wanted to make a commit at least</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{
+        fontSize:20,
+        fontFamily: 'outfit-medium'
+      }}>Just wanted to make a commit at least </Text>
       <StatusBar style="auto" />
     </View>
   );
