77ca1fe00b9f144826b18dd6f56afa82c16fa5c7
devkobby24 <nanakobby2002@gmail.com>
2024-06-02 21:59:55 +0000

    progress

--- App/Navigations/TabNavigation.jsx
+++ App/Navigations/TabNavigation.jsx
@@ -1,4 +1,3 @@
-import { View, Text } from 'react-native';
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screen/HomeScreeen/HomeScreen';
