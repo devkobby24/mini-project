a4116967ec2ce5386ab32d034060fcad62ff479a
devkobby24 <nanakobby2002@gmail.com>
2024-06-02 21:32:06 +0000

    progress

--- App/Navigations/TabNavigation.jsx
+++ App/Navigations/TabNavigation.jsx
@@ -1,13 +1,21 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../Screen/HomeScreeen/HomeScreen';
+import FavoriteScreen from '../Screen/FavoriteScreen/FavoriteScreen';
+import ProfileScrren from '../Screen/ProfileScreen/ProfileScrren';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
   return (
     <Tab.Navigator>
-      
+        <Tab.Screen name='home'
+        component={HomeScreen} />
+        <Tab.Screen name='favorite'
+        component={FavoriteScreen} />
+        <Tab.Screen name='profile'
+        component={ProfileScrren} />
     </Tab.Navigator>
   )
 }
---
+++ App/Screen/FavoriteScreen/FavoriteScreen.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function FavoriteScreen() {
+  return (
+    <View>
+      <Text>FavoriteScreen</Text>
+    </View>
+  )
+}
