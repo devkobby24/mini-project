bb8445c4595bda1bfa248d0d4007614d66aeb8b8
devkobby24 <nanakobby2002@gmail.com>
2024-06-10 17:03:47 +0000

    update

--- App/Screen/HomeScreeen/Header.jsx
+++ App/Screen/HomeScreeen/Header.jsx
@@ -1,18 +1,43 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { useUser } from '@clerk/clerk-expo'
+import { AntDesign } from '@expo/vector-icons';
+import Colors from '../../Utils/Colors';
 
 export default function Header() {
     const {user}=useUser();
   return (
-    <View>
+    <View style={styles.container}>
       <Image source={{uri:user?.imageUrl}}
-        style={{width:45,height:45,borderRadius:99}}      
+        style={styles.userImage}      
       />
       
       <Image source={require('../../../assets/images/logo1.png')} 
-        style={{width:200,height:45,objectFit:'contain'}}
+        style={styles.logo}
       />
+      <AntDesign name="filter" size={26} color="black" />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    logo:{
+        width:200,
+        height:45,
+        objectFit:'contain'
+    },
+    userImage:{
+        width:45,
+        height:45,
+        borderRadius:99
+    },
+    container:{
+        display:'flex',
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        backgroundColor:Colors.TRANSPARENT
+    }
+
+
+})
--- App/Utils/Colors.js
+++ App/Utils/Colors.js
@@ -2,5 +2,6 @@ export default{
     PRIMARY:"#0BC224",
     GRAY:'#898989',
     WHITE:"#FFFFFF",
-    BLACK:'#000000'
+    BLACK:'#000000',
+    TRANSPARENT:'#FFFFFF87'
 }
