afeb66db5cbec2d24bd12472c0082cd44332f030
devkobby24 <nanakobby2002@gmail.com>
2024-06-18 17:38:59 +0000

    update

--- App/Screen/HomeScreeen/SearchBar.jsx
+++ App/Screen/HomeScreeen/SearchBar.jsx
@@ -26,7 +26,7 @@ export default function SearchBar() {
 const styles = StyleSheet.create({
     searchbar:{
         borderRadius:6,
-        backgroundColor: Colors.TRANSPARENT,
+        backgroundColor: Colors.WHITE,
         display:'flex',
         flexDirection:'row',
         marginTop:15,
