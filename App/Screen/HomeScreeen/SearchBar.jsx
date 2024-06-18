696c1f3675cceeef6d7e78e973240a77067a8d2a
devkobby24 <nanakobby2002@gmail.com>
2024-06-18 18:30:18 +0000

    update

--- App/Screen/HomeScreeen/SearchBar.jsx
+++ App/Screen/HomeScreeen/SearchBar.jsx
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import Colors from '../../Utils/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function SearchBar({searchedLocation}) {
   return (
     <View style={styles.searchbar}>
+        <Ionicons name="location-sharp" size={24} color={Colors.GRAY} style={{paddingTop:10}}/>
       <GooglePlacesAutocomplete
       placeholder='Search EV Charging Station'
       fetchDetails={true}
@@ -16,7 +18,8 @@ export default function SearchBar({searchedLocation}) {
         searchedLocation(details?.geometry?.location)
       }}
       query={{
-        key: 'AIzaSyDGAs9NQQD_LzlJ88zSYWDyWMwQXkOMo8M',
+        //key: 'AIzaSyDGAs9NQQD_LzlJ88zSYWDyWMwQXkOMo8M',
+        key: 'AIzaSyB9ctiAb-J9CZil_ZlpAg3ZOXpxwudHlNw',
         language: 'en',
       }}
     />
@@ -32,5 +35,9 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         marginTop:15,
         paddingHorizontal:5
+    },
+    locationIcon:{
+
     }
+
 })
