2ceedaded1e48b71593009e9b5975c9d1ea514eb
devkobby24 <nanakobby2002@gmail.com>
2024-06-18 17:07:50 +0000

    update

--- App/Screen/HomeScreeen/SearchBar.jsx
+++ App/Screen/HomeScreeen/SearchBar.jsx
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 export default function SearchBar() {
   return (
     <View>
-      <Text>SearchBar</Text>
+      <GooglePlacesAutocomplete
+      placeholder='Search'
+      enablePoweredByContainer={false}
+      onPress={(data, details = null) => {
+        // 'details' is provided when fetchDetails = true
+        console.log(data, details);
+      }}
+      query={{
+        key: 'AIzaSyDGAs9NQQD_LzlJ88zSYWDyWMwQXkOMo8M',
+        language: 'en',
+      }}
+    />
     </View>
   )
 }
