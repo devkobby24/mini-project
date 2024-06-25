b760eb0341354d4c6f55263dcddb4cf9c8210bc4
devkobby24 <nanakobby2002@gmail.com>
2024-06-25 09:37:11 +0000

    update

--- App.js
+++ App.js
@@ -87,7 +87,7 @@ export default function App() {
         <SignedOut>
           <LoginScreen />
         </SignedOut>
-        
+          
         <StatusBar style="auto" />   
       </SafeAreaView>
       </UserLocationContext.Provider>
