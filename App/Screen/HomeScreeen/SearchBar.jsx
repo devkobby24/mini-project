259df027c320ac27d53d2a33abef8481b7d8af3d
devkobby24 <nanakobby2002@gmail.com>
2024-07-08 10:16:41 +0000

    update

--- App/Screen/HomeScreeen/SearchBar.jsx
+++ App/Screen/HomeScreeen/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,View } from 'react-native';
 import React from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import Colors from '../../Utils/Colors';
