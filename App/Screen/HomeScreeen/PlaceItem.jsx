669ee200c771a9c2af34bad97f73d5ab992bbe30
devkobby24 <nanakobby2002@gmail.com>
2024-07-24 21:51:19 +0000

    update

--- App/Screen/HomeScreeen/PlaceItem.jsx
+++ App/Screen/HomeScreeen/PlaceItem.jsx
@@ -1,88 +1,149 @@
-import { View, Text, Image, Dimensions, Pressable, ToastAndroid } from 'react-native'
-import React from 'react'
-import Colors from '../../Utils/Colors'
-import GlobalApi from '../../Utils/GlobalApi'
-import { LinearGradient } from 'expo-linear-gradient';
-import { FontAwesome6 } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
+import {
+  View,
+  Text,
+  Image,
+  Dimensions,
+  Pressable,
+  ToastAndroid,
+  Platform,
+  Linking,
+} from "react-native";
+import React from "react";
+import Colors from "../../Utils/Colors";
+import GlobalApi from "../../Utils/GlobalApi";
+import { LinearGradient } from "expo-linear-gradient";
+import { FontAwesome6 } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import { getFirestore } from "firebase/firestore";
-import { app } from '../../Utils/FirebaseConfig';
-import { doc, setDoc } from "firebase/firestore";
-import { useUser } from '@clerk/clerk-expo';
+import { app } from "../../Utils/FirebaseConfig";
+import { doc, setDoc, deleteDoc } from "firebase/firestore";
+import { useUser } from "@clerk/clerk-expo";
 
-export default function PlaceItem({place}) {
-
-  const PLACE_PHOTO_BASE_URL="https://places.googleapis.com/v1/"
-  const {user}=useUser();
+export default function PlaceItem({ place, isFav, markedFav }) {
+  const PLACE_PHOTO_BASE_URL = "https://places.googleapis.com/v1/";
+  const { user } = useUser();
   const db = getFirestore(app);
-  const onSetFav=async(place)=>{
-    await setDoc(doc(db, "ev-fav-place", (place.id).toString()),
-      { place:place,
-        email:user?.primaryEmailAddress?.emailAddress
-      }
-    );
-    ToastAndroid.show('Favorite Added!', ToastAndroid.TOP)
+  const onSetFav = async (place) => {
+    await setDoc(doc(db, "ev-fav-place", place.id.toString()), {
+      place: place,
+      email: user?.primaryEmailAddress?.emailAddress,
+    });
+    markedFav();
+    ToastAndroid.show("Favorite Added!", ToastAndroid.TOP);
+  };
+
+  const onRemoveFav=async(placeId)=>{
+    await deleteDoc(doc(db, "ev-fav-place", placeId.toString()));
+    ToastAndroid.show("Favorite Removed!", ToastAndroid.TOP);
+    markedFav();
+  }
+
+  const onDirectionClick=()=>{
+    const url=Platform.select({
+      ios:"maps:"+place?.location?.latitude+","+place?.location?.longitude+"?q"+place?.formattedAddress,
+      android:"geo:"+place?.location?.latitude+","+place?.location?.longitude+"?q"+place?.formattedAddress 
+    });
+
+    Linking.openURL(url);
   }
 
   return (
     <View
-        style={{width:Dimensions.get('screen').width*0.9,
-            backgroundColor:Colors.WHITE,
-            margin:5,
-            borderRadius:10}}
+      style={{
+        width: Dimensions.get("screen").width * 0.95,
+        backgroundColor: Colors.WHITE,
+        margin: 5,
+        marginLeft: 12,
+        borderRadius: 10,
+      }}
     >
-      <LinearGradient 
-        colors={['transparent','#ffffff','#ffffff']} 
-      >
-        <Pressable style={{position:'absolute',right:0,margin:5}} onPress={()=>onSetFav(place)}>
-          <Ionicons name="heart-outline" size={30} color="white" />
+      <LinearGradient colors={["transparent", "#ffffff", "#ffffff"]}>
+        {!isFav? <Pressable
+          style={{ position: "absolute", right: 0, margin: 5 }}
+          onPress={() => onSetFav(place)}
+        >
+          <Ionicons name="heart-outline" size={30} color="black" />
         </Pressable>
-      <Image source={
-        place?.photos?
-        {uri:PLACE_PHOTO_BASE_URL+place?.photos[0]?.name+
-        "media?key="+ GlobalApi.API_KEY+"&maxHeightPx=800&maxWidthPx=1200"}
-        :require('./../../../assets/images/Station.jpg')} 
-        style={{width:'100%',
-                borderRadius:10,
-                height:150,
-                zIndex:-1}}
-      />
-      <View style={{padding:5}}>
-    <Text style={{
-        color:Colors.BLACK,
-        fontSize:23,
-        fontFamily:'outfit-medium'
-    }}>{place.displayName?.text}</Text>
-    <Text style={{
-      color:Colors.GRAY,
-      fontFamily: 'outfit'
-    }}>{place.shortFormattedAddress}</Text>
+          :
+        <Pressable
+          style={{ position: "absolute", right: 0, margin: 5 }}
+          onPress={() => onRemoveFav(place.id)}
+        >
+          <Ionicons name="heart-sharp" size={30} color="red" />
+        </Pressable> }
 
-      <View style={{
-        marginTop:5,
-      }}>
-        <Text style={{
-          fontFamily:'outfit',
-          color:Colors.GRAY,
-          fontSize:17
-        }}>Connectors</Text>
-        <Text style={{
-          fontFamily:'outfit-medium',
-          fontSize:20,
-          marginTop:2
-        }}>{place.evChargeOptions?.connectorCount} Points</Text>
-        <View style={{
-          padding:12,
-          backgroundColor:Colors.PRIMARY,
-          borderRadius:6,
-          // paddingHorizontal:14,
-          marginLeft: '85%'
-        }}> 
-          <FontAwesome6 name="location-arrow" size={25} color="white" />
+        <Image
+          source={
+            place?.photos
+              ? {
+                  uri:
+                    PLACE_PHOTO_BASE_URL +
+                    place?.photos[0]?.name +
+                    "media?key=" +
+                    GlobalApi.API_KEY +
+                    "&maxHeightPx=800&maxWidthPx=1200",
+                }
+              : require("./../../../assets/images/Station.jpg")
+          }
+          style={{ width: "100%", borderRadius: 10, height: 150, zIndex: -1 }}
+        />
+        <View style={{ padding: 5 }}>
+          <Text
+            style={{
+              color: Colors.BLACK,
+              fontSize: 23,
+              fontFamily: "outfit-medium",
+            }}
+          >
+            {place.displayName?.text}
+          </Text>
+          <Text
+            style={{
+              color: Colors.GRAY,
+              fontFamily: "outfit",
+            }}
+          >
+            {place.shortFormattedAddress}
+          </Text>
+
+          <View
+            style={{
+              marginTop: 5,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "outfit",
+                color: Colors.GRAY,
+                fontSize: 17,
+              }}
+            >
+              Connectors
+            </Text>
+            <Text
+              style={{
+                fontFamily: "outfit-medium",
+                fontSize: 20,
+                marginTop: 2,
+              }}
+            >
+              {place.evChargeOptions?.connectorCount} Points
+            </Text>
+            <Pressable
+              onPress={()=>onDirectionClick()}
+              style={{
+                padding: 12,
+                backgroundColor: Colors.PRIMARY,
+                borderRadius: 6,
+                // paddingHorizontal:14,
+                marginLeft: "85%",
+              }}
+            >
+              <FontAwesome6 name="location-arrow" size={25} color="white" />
+            </Pressable>
+          </View>
         </View>
-      </View>
-      </View>
       </LinearGradient>
     </View>
-  )
+  );
 }
--- App/Screen/HomeScreeen/PlaceListView.jsx
+++ App/Screen/HomeScreeen/PlaceListView.jsx
@@ -1,12 +1,18 @@
 import { View, Text, FlatList, Dimensions } from 'react-native'
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import PlaceItem from './PlaceItem'
 import { SelectMarkerContext } from '../../Context/SelectMarkerContext';
+import { app } from '../../Utils/FirebaseConfig';
+import { getFirestore } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { useUser } from '@clerk/clerk-expo';
 
 export default function PlaceListView({placeList}) {
     // console.log('***',placeList);
     const flatListRef = useRef(null);
     const {selectedMarker, setSelectedMarker}=useContext(SelectMarkerContext)
+    const {user}=useUser();
+    const [favList,setFavList]=useState([]);
 
     // useEffect(()=>{
     //     selectedMarker&&scrollToIndex(selectedMarker)
@@ -23,7 +29,7 @@ export default function PlaceListView({placeList}) {
   
     const scrollToIndex = (index) => {
       if (index >= 0 && index < placeList.length) {
-        flatListRef.current?.scrollToIndex({ animated: true, index });
+        flatListRef.current?.scrollToIndex({ animated:true,index });
       }
     }
 
@@ -32,6 +38,30 @@ export default function PlaceListView({placeList}) {
       offset:Dimensions.get('window').width*index,
       index
     });
+    //Get Data from firestore
+    const db = getFirestore(app);
+    
+    useEffect(()=>{
+      user&&getFav();
+    },[user])
+    const getFav=async()=>{
+      setFavList([])
+      const q = query(collection(db, "ev-fav-place"),
+       where("email", "==", user?.primaryEmailAddress?.emailAddress));
+
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+      setFavList(favList=>[...favList,doc.data()]);
+      });
+    }
+
+    const isFav=(place)=>{
+      const result=favList.find(item=>item.place.id== place.id)
+      console.log(result)
+      return result?true:false;
+    }
 
   return (
     <View>
@@ -44,7 +74,10 @@ export default function PlaceListView({placeList}) {
         showsHorizontalScrollIndicator={false}
         renderItem={({item,index})=>(
             <View key={index}>
-                <PlaceItem place={item} />
+                <PlaceItem place={item}
+                  isFav={isFav(item)} 
+                  markedFav={()=>getFav()}               
+                />
             </View>
         )}
       />
